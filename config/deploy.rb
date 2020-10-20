6a2391abbdc20ad47ace8f419385b51382199dcc
sugimotojin <tatoonashi0203@icloud.com>
2020-10-20 15:39:34 +0900

    config.deply.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, "git@example.com:JiM707502345/errors_original_app.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
+set :rbenv_ruby, '2.6.5'
 
 set :ssh_options, auth_methods: ['publickey'],
                                   keys: ['~/.ssh/toumeisyoujyo0203.pem'] 
